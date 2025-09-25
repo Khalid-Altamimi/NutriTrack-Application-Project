639418a9f44da20a734ee340942e305c9be9ac6c
Khalid Altamimi <khalida.altamimi71@gmail.com>
2025-09-25 16:07:30 +0300

    I only did the routes

---
+++ routes/meals.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const mealsController = require('../controllers/meals');
+const isSignedIn = require('../middlewares/is-signed-in');
+
+router.use(isSignedIn);
+
+router.get('/', mealsController.index);
+
+
+router.get('/new', mealsController.new);
+
+
+router.post('/', mealsController.create);
+
+
+router.get('/:id/edit', mealsController.edit);
+
+
+router.put('/:id', mealsController.update);
+
+
+router.delete('/:id', mealsController.destroy);
+
+
+module.exports = router;
+
+
+
+
